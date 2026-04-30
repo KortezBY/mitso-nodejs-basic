54a2942d085947361d780f482a19aa89475a8fc0
student <student@mitso.by>
2026-04-30 13:21:16 +0300

    env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'MITSO_';
+    const entries = Object.entries(process.env)
+        .filter(([key]) => key.startsWith(prefix))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+    
+    if (entries) {
+        console.log(entries);
+    }
 };
 
 parseEnv();
