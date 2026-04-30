67f9868c306e49efc9b0e3133f86be210cf10ada
student <student@mitso.by>
2026-04-30 13:24:21 +0300

    streamswrite

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'fileToWrite.txt');
+    const writable = createWriteStream(filePath, 'utf-8');
+    process.stdin.pipe(writable);
 };
 
 await write();
