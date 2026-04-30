62ec411511a87347e207d56a8893ed37f6d1f748
student <student@mitso.by>
2026-04-30 13:23:48 +0300

    calsHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'fileToCalculateHashFor.txt');
+    const content = await readFile(filePath);
+    const hash = createHash('sha256').update(content).digest('hex');
+    console.log(hash);
 };
 
 await calculateHash();
