d10412e07c6240ea812010e6f2a03d1996ace398
student <student@mitso.by>
2026-04-30 13:20:05 +0300

    create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import { writeFile, mkdir } from 'fs/promises';
+import { existsSync } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const folderPath = join(__dirname, 'files');
+    const filePath = join(folderPath, 'fresh.txt');
+
+    if (existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await mkdir(folderPath, { recursive: true });
+        await writeFile(filePath, 'I am fresh and young', 'utf8');
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
