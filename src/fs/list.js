f33248ff7742bd139cb85ab0e97dc2a46813c491
student <student@mitso.by>
2026-04-30 13:20:55 +0300

    list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { readdir, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const folder = join(__dirname, 'files');
+
+    try {
+        await access(folder);
+    } catch {
+        throw new Error('FS operation failed');
+    }
+
+    const files = await readdir(folder);
+    console.log(files);
 };
 
 await list();
