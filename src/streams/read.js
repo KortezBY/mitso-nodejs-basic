a37c491ac87b41d4d362f2074434f54690d29258
student <student@mitso.by>
2026-04-30 13:24:07 +0300

    streamsread

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'fileToRead.txt');
+    const readable = createReadStream(filePath, 'utf-8');
+    readable.pipe(process.stdout);
 };
 
 await read();
